eeb6d9dff7ae312a81665965a3f7d8991a134d37
johny koudsi <johnykodsy@gmail.com>
2023-05-14 20:51:51 +0300

    UniformQuantization

--- src/color_quantization_algorithms/UniformQuantization.java
+++ src/color_quantization_algorithms/UniformQuantization.java
@@ -6,6 +6,7 @@ public class UniformQuantization {
 
     public static BufferedImage quantize(BufferedImage image, int numColors) {
         // Calculate the color quantization level
+        //Math.ceil() round up the logarithm of the number of colors to the nearest integer
         int levels = (int) Math.ceil(Math.log(numColors) / Math.log(2));
 
         // Calculate the color quantization interval
@@ -21,6 +22,9 @@ public class UniformQuantization {
                 Color color = new Color(image.getRGB(x, y));
 
                 // Calculate the quantized color
+                //calculates the quantized color for the pixel. This is done by dividing
+                // each component of the color by the color quantization interval
+                // and then rounding down to the nearest integer
                 int red = (color.getRed() / interval) * interval;
                 int green = (color.getGreen() / interval) * interval;
                 int blue = (color.getBlue() / interval) * interval;
